8659a861d68e3ad9062623cdd4c4726c1a823147
99Choo <chush4649@gmail.com>
2025-04-12 19:18:53 +0900

    front-end

--- front_end_frame/07/Counter.jsx
+++ front_end_frame/07/Counter.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Counter(props) {
   const [count, setCount] = useState(0);
+
+  // componentDidMount, componentDidUpdate와 비슷하게 작동.
+  useEffect(() => {
+    // 브라우저 API를 사용해서 document의 title을 업데이트 합니다.
+    document.title = `총 ${count}번 클릭했습니다.`;
+  },[]);
 
   return (
     <div>
